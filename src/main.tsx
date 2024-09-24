b360c750dbd8e4b5e706b1981684b081f7c5a5af
Justyna Gruba <justyna.gruba@deployed.pl>
2024-09-24 08:24:37 +0200

    Improvements after frontend meeting

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
 
 import App from './App';
 import './index.css';
@@ -10,6 +11,8 @@ const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement!);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
