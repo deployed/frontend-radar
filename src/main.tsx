9f71358351909d92cda2cec2f245478b28cc40d3
Justyna Gruba <justyna.gruba@deployed.pl>
2024-10-18 09:29:41 +0200

    layout improved.

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 
 import App from './App';
-import './index.scss';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 
